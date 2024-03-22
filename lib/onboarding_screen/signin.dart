4de8f1de3795eac660d266fb2600fe4ef0dedc4d
GABRIEL GIDEON BASSEY <140850609+Famous-guy@users.noreply.github.com>
2024-03-22 02:25:34 +0100

    minor bugs fixed

--- lib/onboarding_screen/signin.dart
+++ lib/onboarding_screen/signin.dart
@@ -240,6 +240,8 @@ class SignIn extends StatefulWidget {
   State<SignIn> createState() => _SignInState();
 }
 
+bool _isDarkMode = false;
+
 class _SignInState extends State<SignIn> {
   final apiKeyController = TextEditingController();
   bool _isApiKeyValid = true;
@@ -247,180 +249,189 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
+    final Brightness brightness = MediaQuery.of(context).platformBrightness;
     // Retrieve the current theme from the context
     final ThemeData theme = Theme.of(context);
+    _isDarkMode = brightness == Brightness.dark;
+    String imageAssetPath = _isDarkMode
+        ? 'assets/images/100Paywhite.png'
+        : 'assets/images/100Payblack.png';
 
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.only(top: 40),
-              child: Image.asset(
-                'assets/images/100Pay${theme.brightness == Brightness.dark ? 'white' : 'black'}.png',
-                width: 100,
+    return MaterialApp(
+      theme: _isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 40),
+                child: Image.asset(
+                  imageAssetPath,
+                  // 'assets/images/100Pay${theme.brightness == Brightness.dark ? 'white' : 'black'}.png',
+                  width: 100,
+                ),
               ),
-            ),
-            SizedBox(height: 120),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Connect Your 100pay Account',
-                    style: TextStyle(
-                      fontFamily: 'space_grotesk',
-                      fontSize: 20,
-                      color: theme.colorScheme.onSurface,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Enter your account ID',
-                    style: TextStyle(
-                      fontFamily: 'space_grotesk',
-                      fontSize: 14,
-                      color: theme.colorScheme.onSurface.withOpacity(0.6),
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  SizedBox(height: 8),
-                  _buildApiKeyField(),
-                ],
-              ),
-            ),
-
-            Align(
-              alignment: Alignment.bottomRight,
-              child: SizedBox(
-                height: 48,
-                width: 136,
-                child: _isLoading
-                    ? ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15)),
-                          backgroundColor: Color.fromRGBO(242, 8, 49, 0.7),
-                        ),
-                        child: CircularProgressIndicator(
-                          valueColor:
-                              AlwaysStoppedAnimation<Color>(Colors.white),
-                        ),
-                      )
-                    : ElevatedButton(
-                        onPressed: () async {
-                          setState(() {
-                            _isLoading = true;
-
-                            Future.delayed(Duration(seconds: 5), () async {
-                              if (apiKeyController.text.isEmpty) {
-                                _showErrorMessage();
-                                _isLoading = false;
-                                return;
-                              }
-
-                              _isApiKeyValid =
-                                  _validateApiKey(apiKeyController.text);
-
-                              await Provider.of<UserDataProvider>(
-                                context,
-                                listen: false,
-                              ).connectPOS(apiKeyController.text);
-
-                              // Navigate to the next screen
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => Pay100(),
-                                ),
-                              );
-
-                              _isLoading = false;
-                            });
-                          });
-                        },
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15)),
-                          backgroundColor: Color.fromRGBO(242, 8, 49, 1),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              'Continue',
-                              style: TextStyle(
-                                  color: Color.fromRGBO(255, 255, 255, 1),
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 14),
-                            ),
-                            Icon(
-                              Icons.arrow_forward_ios,
-                              size: 16,
-                              weight: 100,
-                              color: Colors.white,
-                            ),
-                          ],
-                        ),
+              SizedBox(height: 120),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Connect Your 100pay Account',
+                      style: TextStyle(
+                        fontFamily: 'space_grotesk',
+                        fontSize: 20,
+                        color: theme.colorScheme.onSurface,
+                        fontWeight: FontWeight.w700,
                       ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'Enter your account ID',
+                      style: TextStyle(
+                        fontFamily: 'space_grotesk',
+                        fontSize: 14,
+                        color: theme.colorScheme.onSurface.withOpacity(0.6),
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    _buildApiKeyField(),
+                  ],
+                ),
               ),
-            ),
 
-            // Align(
-            //   alignment: Alignment.bottomRight,
-            //   child: SizedBox(
-            //     height: 48,
-            //     width: 136,
-            //     child: _isLoading
-            //         ? ElevatedButton(
-            //             onPressed: () {},
-            //             style: ElevatedButton.styleFrom(
-            //               shape: RoundedRectangleBorder(
-            //                   borderRadius: BorderRadius.circular(15)),
-            //               backgroundColor: Color.fromRGBO(242, 8, 49, 0.7),
-            //             ),
-            //             child: CircularProgressIndicator(
-            //               valueColor:
-            //                   AlwaysStoppedAnimation<Color>(Colors.white),
-            //             ),
-            //           )
-            //         : ElevatedButton(
-            //             onPressed: () async {
-            //               // setState(() {
-            //               //   _isLoading = true;
-            //               //   // Your logic for handling API key entry and navigation
-            //               // });
-            //             },
-            //             style: ElevatedButton.styleFrom(
-            //               shape: RoundedRectangleBorder(
-            //                   borderRadius: BorderRadius.circular(15)),
-            //               backgroundColor: Color.fromRGBO(242, 8, 49, 0.7),
-            //             ),
-            //             child: Row(
-            //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //               children: [
-            //                 Text(
-            //                   'Continue',
-            //                   style: TextStyle(
-            //                     color: Colors.white,
-            //                     fontWeight: FontWeight.w600,
-            //                     fontSize: 14,
-            //                   ),
-            //                 ),
-            //                 Icon(
-            //                   Icons.arrow_forward_ios,
-            //                   size: 16,
-            //                   color: Colors.white,
-            //                 ),
-            //               ],
-            //             ),
-            //           ),
-            //   ),
-            // ),
-          ],
+              Align(
+                alignment: Alignment.bottomRight,
+                child: SizedBox(
+                  height: 48,
+                  width: 136,
+                  child: _isLoading
+                      ? ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15)),
+                            backgroundColor: Color.fromRGBO(242, 8, 49, 0.7),
+                          ),
+                          child: CircularProgressIndicator(
+                            valueColor:
+                                AlwaysStoppedAnimation<Color>(Colors.white),
+                          ),
+                        )
+                      : ElevatedButton(
+                          onPressed: () async {
+                            setState(() {
+                              _isLoading = true;
+
+                              Future.delayed(Duration(seconds: 5), () async {
+                                if (apiKeyController.text.isEmpty) {
+                                  _showErrorMessage();
+                                  _isLoading = false;
+                                  return;
+                                }
+
+                                _isApiKeyValid =
+                                    _validateApiKey(apiKeyController.text);
+
+                                await Provider.of<UserDataProvider>(
+                                  context,
+                                  listen: false,
+                                ).connectPOS(apiKeyController.text);
+
+                                // Navigate to the next screen
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => Pay100(),
+                                  ),
+                                );
+
+                                _isLoading = false;
+                              });
+                            });
+                          },
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15)),
+                            backgroundColor: Color.fromRGBO(242, 8, 49, 1),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Continue',
+                                style: TextStyle(
+                                    color: Color.fromRGBO(255, 255, 255, 1),
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 14),
+                              ),
+                              Icon(
+                                Icons.arrow_forward_ios,
+                                size: 16,
+                                weight: 100,
+                                color: Colors.white,
+                              ),
+                            ],
+                          ),
+                        ),
+                ),
+              ),
+
+              // Align(
+              //   alignment: Alignment.bottomRight,
+              //   child: SizedBox(
+              //     height: 48,
+              //     width: 136,
+              //     child: _isLoading
+              //         ? ElevatedButton(
+              //             onPressed: () {},
+              //             style: ElevatedButton.styleFrom(
+              //               shape: RoundedRectangleBorder(
+              //                   borderRadius: BorderRadius.circular(15)),
+              //               backgroundColor: Color.fromRGBO(242, 8, 49, 0.7),
+              //             ),
+              //             child: CircularProgressIndicator(
+              //               valueColor:
+              //                   AlwaysStoppedAnimation<Color>(Colors.white),
+              //             ),
+              //           )
+              //         : ElevatedButton(
+              //             onPressed: () async {
+              //               // setState(() {
+              //               //   _isLoading = true;
+              //               //   // Your logic for handling API key entry and navigation
+              //               // });
+              //             },
+              //             style: ElevatedButton.styleFrom(
+              //               shape: RoundedRectangleBorder(
+              //                   borderRadius: BorderRadius.circular(15)),
+              //               backgroundColor: Color.fromRGBO(242, 8, 49, 0.7),
+              //             ),
+              //             child: Row(
+              //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //               children: [
+              //                 Text(
+              //                   'Continue',
+              //                   style: TextStyle(
+              //                     color: Colors.white,
+              //                     fontWeight: FontWeight.w600,
+              //                     fontSize: 14,
+              //                   ),
+              //                 ),
+              //                 Icon(
+              //                   Icons.arrow_forward_ios,
+              //                   size: 16,
+              //                   color: Colors.white,
+              //                 ),
+              //               ],
+              //             ),
+              //           ),
+              //   ),
+              // ),
+            ],
+          ),
         ),
       ),
     );
