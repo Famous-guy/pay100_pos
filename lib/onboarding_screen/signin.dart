56fc5aec6de4368bfba6a5db7f7889f88fd7173d
GABRIEL GIDEON BASSEY <140850609+Famous-guy@users.noreply.github.com>
2024-03-22 01:33:01 +0100

    added

--- lib/onboarding_screen/signin.dart
+++ lib/onboarding_screen/signin.dart
@@ -252,144 +252,142 @@ class _SignInState extends State<SignIn> {
       debugShowCheckedModeBanner: false,
       theme: _isDarkMode ? ThemeData.dark() : ThemeData.light(),
       home: Scaffold(
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Switch(
+                    value: _isDarkMode,
+                    onChanged: (value) {
+                      setState(() {
+                        _isDarkMode = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                child: Image.asset(
+                  imageAssetPath,
+                  width: 100,
+                ),
+              ),
+              SizedBox(
+                height: 150,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Switch(
-                      value: _isDarkMode,
-                      onChanged: (value) {
-                        setState(() {
-                          _isDarkMode = value;
-                        });
-                      },
+                    Text(
+                      'Connect To Your wallet',
+                      style: TextStyle(
+                        fontFamily: 'space_grotesk',
+                        fontSize: 20,
+                        color: _isDarkMode ? Colors.white : Color(0xff4B5563),
+                        fontWeight: FontWeight.w700,
+                      ),
                     ),
+                    SizedBox(height: 20),
+                    Text(
+                      'Enter your wallet account ID',
+                      style: TextStyle(
+                        fontFamily: 'space_grotesk',
+                        fontSize: 14,
+                        color: _isDarkMode ? Colors.white : Color(0xff344054),
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    _buildApiKeyField(),
                   ],
                 ),
-                Container(
-                  margin: EdgeInsets.only(top: 10),
-                  child: Image.asset(
-                    imageAssetPath,
-                    width: 100,
-                  ),
-                ),
-                SizedBox(
-                  height: 150,
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    // mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Connect To Your wallet',
-                        style: TextStyle(
-                          fontFamily: 'space_grotesk',
-                          fontSize: 20,
-                          color: _isDarkMode ? Colors.white : Color(0xff4B5563),
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      Text(
-                        'Enter your wallet account ID',
-                        style: TextStyle(
-                          fontFamily: 'space_grotesk',
-                          fontSize: 14,
-                          color: _isDarkMode ? Colors.white : Color(0xff344054),
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      SizedBox(height: 8),
-                      _buildApiKeyField(),
-                    ],
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: SizedBox(
-                    height: 48,
-                    width: 136,
-                    child: _isLoading
-                        ? ElevatedButton(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(15)),
-                              backgroundColor: Color.fromRGBO(242, 8, 49, 0.7),
-                            ),
-                            child: CircularProgressIndicator(
-                              valueColor:
-                                  AlwaysStoppedAnimation<Color>(Colors.white),
-                            ),
-                          )
-                        : ElevatedButton(
-                            onPressed: () async {
-                              setState(() {
-                                _isLoading = true;
-
-                                Future.delayed(Duration(seconds: 5), () async {
-                                  if (apiKeyController.text.isEmpty) {
-                                    _showErrorMessage();
-                                    _isLoading = false;
-                                    return;
-                                  }
-
-                                  _isApiKeyValid =
-                                      _validateApiKey(apiKeyController.text);
-
-                                  if (_isApiKeyValid) {
-                                    // Save API key to local storage
-                                    await Provider.of<UserDataProvider>(
-                                      context,
-                                      listen: false,
-                                    ).connectPOS(apiKeyController.text);
-
-                                    // Navigate to the next screen
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => Pay100(),
-                                      ),
-                                    );
-                                  }
-
-                                  _isLoading = false;
-                                });
-                              });
-                            },
-                            style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(15)),
-                              backgroundColor: Color.fromRGBO(242, 8, 49, 1),
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  'Continue',
-                                  style: TextStyle(
-                                      color: Color.fromRGBO(255, 255, 255, 1),
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: 14),
-                                ),
-                                Icon(
-                                  Icons.arrow_forward_ios,
-                                  size: 16,
-                                  weight: 100,
-                                  color: Colors.white,
-                                ),
-                              ],
-                            ),
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: SizedBox(
+                  height: 48,
+                  width: 136,
+                  child: _isLoading
+                      ? ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15)),
+                            backgroundColor: Color.fromRGBO(242, 8, 49, 0.7),
                           ),
-                  ),
+                          child: CircularProgressIndicator(
+                            valueColor:
+                                AlwaysStoppedAnimation<Color>(Colors.white),
+                          ),
+                        )
+                      : ElevatedButton(
+                          onPressed: () async {
+                            setState(() {
+                              _isLoading = true;
+
+                              Future.delayed(Duration(seconds: 5), () async {
+                                if (apiKeyController.text.isEmpty) {
+                                  _showErrorMessage();
+                                  _isLoading = false;
+                                  return;
+                                }
+
+                                _isApiKeyValid =
+                                    _validateApiKey(apiKeyController.text);
+
+                                if (_isApiKeyValid) {
+                                  // Save API key to local storage
+                                  await Provider.of<UserDataProvider>(
+                                    context,
+                                    listen: false,
+                                  ).connectPOS(apiKeyController.text);
+
+                                  // Navigate to the next screen
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => Pay100(),
+                                    ),
+                                  );
+                                }
+
+                                _isLoading = false;
+                              });
+                            });
+                          },
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15)),
+                            backgroundColor: Color.fromRGBO(242, 8, 49, 1),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Continue',
+                                style: TextStyle(
+                                    color: Color.fromRGBO(255, 255, 255, 1),
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 14),
+                              ),
+                              Icon(
+                                Icons.arrow_forward_ios,
+                                size: 16,
+                                weight: 100,
+                                color: Colors.white,
+                              ),
+                            ],
+                          ),
+                        ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
